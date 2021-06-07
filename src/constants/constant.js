8a755c656c29e57f562519148455655569593037
Trần Hào <haotn.fxp@outlook.com>
2021-06-07 22:34:05 +0700

    edit api

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://demo1511401.mockable.io";
+export const BASE_URL = "https://5fdb546891f19e00173341c7.mockapi.io/api/";
 // export const BASE_URL = "http://localhost:3004";
 export const TOKEN = "token";
 export const REFRESH_TOKEN = "refresh_token";
