33c4c9bd010cf7dc4cdb16c6aa2f2dd6abea2178
Trần Hào <haotn.fxp@outlook.com>
2021-06-07 22:17:32 +0700

    config url api

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://localhost:3004";
+export const BASE_URL = "http://demo1511401.mockable.io";
+// export const BASE_URL = "http://localhost:3004";
 export const TOKEN = "token";
 export const REFRESH_TOKEN = "refresh_token";
